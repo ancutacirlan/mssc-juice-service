fb75f14dc881ef633a59f3659daa0fa6aaec6d36
Cirlan Ancuta <cirlan.ancutaa@gmail.com>
2020-12-16 17:14:41 +0200

    initial project

---
+++ src/main/java/ancuta/springframework/msscjuiceservice/MsscJuiceServiceApplication.java
@@ -0,0 +1,13 @@
+package ancuta.springframework.msscjuiceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscJuiceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscJuiceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ancuta/springframework/msscjuiceservice/MsscJuiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ancuta.springframework.msscjuiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJuiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
